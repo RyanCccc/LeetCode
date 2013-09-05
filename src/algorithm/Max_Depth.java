5d0c999669863083af97e20517cc67d8c2f43693
RyanCccc <chenrd769@gmail.com>
2013-09-05 11:37:00 -0400

    Added Max_Depth

---
+++ src/algorithm/Max_Depth.java
@@ -0,0 +1,47 @@
+package algorithm;
+
+import java.util.LinkedList;
+
+import util.TreeNode;
+import program.*;
+
+public class Max_Depth implements Testable {
+	public int maxDepth(TreeNode root) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		if (root == null) {
+			return 0;
+		}
+		LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.add(root);
+		int cur_child = 1;
+		int nxt_child = 0;
+		int level = 1;
+		TreeNode node = null;
+		while (!queue.isEmpty()) {
+			node = queue.poll();
+			cur_child--;
+			if (node.left != null) {
+				queue.add(node.left);
+				nxt_child++;
+			}
+			if (node.right != null) {
+				queue.add(node.right);
+				nxt_child++;
+			}
+			if (cur_child == 0) {
+				// End level
+				level++;
+				cur_child = nxt_child;
+				nxt_child = 0;
+			}
+		}
+		return --level;
+	}	
+
+	@Override
+	public boolean test(Object node, int res) {
+		// TODO Auto-generated method stub
+		return maxDepth((TreeNode)node) == res;
+	}
+}
--- src/program/Program.java
+++ src/program/Program.java
@@ -1,5 +1,6 @@
 package program;
 
+import algorithm.Max_Depth;
 import algorithm.Min_Depth;
 import algorithm.Path_Sum;
 import util.*;
@@ -8,10 +9,11 @@ public class Program {
 	public final static int NULL = Integer.MIN_VALUE;
 	public static void main(String[] args){
 		int[] A = new int[]{5,4,8,11,NULL, 13,4,7,2,NULL,NULL,NULL,NULL,NULL,1};
+		int[] C = new int[]{5,4,8,11,NULL, 13,4,7,2,NULL,NULL,NULL,NULL,NULL,1,2};
 		TreeNode nodeA = Tree.generateTree(A);
 		int[] B = new int[]{5};
 		TreeNode nodeB = Tree.generateTree(B);
-		boolean test = false;
+		TreeNode nodeC = Tree.generateTree(C);
 		//Tree.printTree(node);
 		
 		System.out.println("=======Test Path_Sum========");
@@ -21,6 +23,11 @@ public class Program {
 		System.out.println("=======Test Min_Sum========");
 		Min_Depth min_depth = new Min_Depth();
 		result(min_depth, nodeA, 3);
+		System.out.println("=======Test Max_Sum========");
+		Max_Depth max_depth = new Max_Depth();
+		result(max_depth, nodeA, 4);
+		result(max_depth, nodeB, 1);
+		result(max_depth, nodeC, 5);
 	}
 	
 	public static void result(Testable test, Object o, int res){
